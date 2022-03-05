dea223da43421bfe756f8c69543078e91d0a2181
Rafael <rafasc0404@gmail.com>
2022-03-05 12:44:44 -0300

    handles lines longer than limit, updated readme

--- README.md
+++ README.md
@@ -6,12 +6,12 @@ Decided to create this because lately I've been writing a lot of things in plain
 text files and adopted some standards to keep it readable and mantainable. I always
 try to not exceed 80 characters per line and that's quite easy while writing, with
 one command I can freely write with no worries so my editor will break it for me
-when it comes to 80 characters. But the real deal comes when I have to fix something
-or rewrite a part of the text, since it's new words, some lines might be left with
-much free space or another exceed a little and for me to fix that by myself it's
+when it comes to 80 characters. But the dificult thing comes when I have to fix something
+or rewrite a part of the text, since I modified some words, some lines might be left with
+too much space or another exceed a little and for me to fix that by myself it's
 just painfull.
 
-If you want to change the amount of characters of limit (default 80), there is a
+If you want to use it, you can change the amount of characters of limit (default 80), there is a
 variable in the `main.py` file called 'limit', just change that value to what you
 desire.
 
--- main.py
+++ main.py
@@ -10,32 +10,61 @@ lines = open(sys.argv[1], 'r').readlines()
 
 limit = 80
 
+new_file = open('file.txt', 'a')
+
 for i in lines:
     line = i[0:-1].split(" ")
+
+    if len(line) == 1 and line[0] == '': # means it is an empty line
+        new_file.write("\n")
+        continue
+
     chars = 0
     charscount = (len(line) + sum([(chars + len(j)) for j in line])) - 1
-   
-    if len(line) == 1 and line[0] == '': continue # means it is an empty line
-		
-    if charscount > limit: # if it exceeded
+
+    if charscount > limit:
         tmp_charscount = 0
-        fitted_words = []
+        fitted_words = [[]]
         for word in line:
+            if word == '': continue
             if (tmp_charscount + len(word)) > limit: break
             tmp_charscount += (1 + len(word))
-            fitted_words.append(word)
+            fitted_words[0].append(word)
 
-        exceed = [x for x in line if not x in fitted_words or fitted_words.remove(x)]
+        excess = [x for x in line if not x in fitted_words[0]]
 
-        exceed_charcount = 0
-        for word in exceed:
-            exceed_charcount += (1 + len(word))
+        excess_charcount = 0
+        for word in excess: excess_charcount += (1 + len(word))
 
-        # throw exceeds them in the next line
-        #   .how to get the next iteration?
-        #   .pass the anterior line to the next?
+        new_lines = list()
+
+        while excess_charcount > limit:
+            tmp_charscount = 0
+            new_line = list()
+            for word in excess:
+                if word == '': continue
+                if (tmp_charscount + len(word)) > limit: break
+                tmp_charscount += (1 + len(word))
+                new_line.append(word)
+                excess.remove(word)
+            new_lines.append(new_line)
+            excess_charcount = 0
+            for word in excess: excess_charcount += (1 + len(word))
+
+        new_lines.append(excess)
+
+        fitted_words.extend(new_lines)
+
+        for line in fitted_words:
+            linestring = ''
+            for word in line:
+                if word == line[-1]:
+                    linestring += word
+                else:
+                    linestring += (word + " ")
+            linestring += "\n"
+            new_file.write(linestring)
     elif charscount < limit: # if fits more words
         continue
         # get the first word from the next line
         # throw it at the and of this
-    print()
